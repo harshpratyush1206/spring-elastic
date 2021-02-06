245a0f79218115246f640df05c5a94497092dc2f
Harsh Pratyush <Harsh.Pratyush@OAKNORTH.AI>
2021-02-06 22:45:01 +0530

    first commit

---
+++ src/main/java/org/oaknorth/springelastic/SpringelasticApplication.java
@@ -0,0 +1,13 @@
+package org.oaknorth.springelastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringelasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringelasticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/oaknorth/springelastic/SpringelasticApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oaknorth.springelastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringelasticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
